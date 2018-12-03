9b3a2e7c66cf45a142eaea666fdbfecffa24a4de
jhall1990 <jacobhall90@gmail.com>
2018-12-03 16:08:44 -0500

    added better documentation and winner logic

--- app/src/main/java/example/jacob/connectthree/ConnectThree.java
+++ app/src/main/java/example/jacob/connectthree/ConnectThree.java
@@ -1,31 +1,55 @@
 package example.jacob.connectthree;
 
-public class ConnectThree {
-    private int turnCount;
-    public int currentPlayer;
+class ConnectThree {
+    int turnCount;
+    int currentPlayer;
     private int[][] board;
 
-    public ConnectThree() {
+    ConnectThree() {
+        /*
+        Creates a new instance of the ConnectThree game.
+         */
         this.turnCount = 0;
         this.currentPlayer = 1;
         this.board = new int[3][3];
     }
 
-    public int placePiece(int r, int c) {
-        // Place a piece into the board, then check for a winner
-        this.turnCount++;
-        if (this.currentPlayer == 1) {
-            this.board[r][c] = 1;
-            this.currentPlayer = 2;
-        } else {
-            this.board[r][c] = 2;
-            this.currentPlayer = 1;
+    int placePiece(int r, int c) {
+        /*
+        This function attempts to place a piece onto the game board.
+
+        r - The row the piece should be placed in.
+        c - The column the piece should be placed in.
+
+        Returns: -1 if the move is invalid.
+                  0 if there is no winner.
+                  1 if player 1 wins.
+                  2 if player 2 wins.
+         */
+        // Make sure that no other piece in the space.
+        if (this.board[r][c] != 0) {
+            return -1;
         }
+
+        // The move is valid, increase the turn count and
+        // add the piece to the board.
+        this.turnCount++;
+        this.board[r][c] = this.currentPlayer;
+
+        // Check for a winner.
         return this.checkWinner();
     }
 
     private int checkWinner() {
-        if (turnCount >= 2) {
+        /*
+        This function checks if either player has won the game. It checks vertically,
+        horizontally, and diagonally.
+
+        Returns: 0 if there is no winner.
+                 1 if player 1 wins.
+                 2 if player 2 wins.
+         */
+        if (this.turnCount >= 2) {
             int colWin = this.checkColumn();
             if (colWin > 0) {
                 return colWin;
@@ -45,6 +69,13 @@ public class ConnectThree {
     }
 
     private int checkColumn() {
+        /*
+        This functions checks each column for a winner.
+
+        Returns: 0 if there is no winner.
+                 1 if player 1 wins.
+                 2 if player 2 wins.
+         */
         for (int i = 0; i < 3; i++) {
             if ((this.board[i][0] == this.board[i][1]) && (this.board[i][0] == this.board[i][2])) {
                 return this.board[i][0];
@@ -54,6 +85,13 @@ public class ConnectThree {
     }
 
     private int checkRow() {
+        /*
+        This function checks each row for a winner.
+
+        Returns: 0 if there is no winner.
+                 1 if player 1 wins.
+                 2 if player 2 wins.
+         */
         for (int i = 0; i < 3; i++) {
             if ((this.board[0][i] == this.board[1][i]) && (this.board[0][i] == this.board[2][i])) {
                 return this.board[0][i];
@@ -63,6 +101,13 @@ public class ConnectThree {
     }
 
     private int checkDiagonal() {
+        /*
+        This function checks each diagonal for a winner.
+
+        Returns: 0 if there is no winner.
+                 1 if player 1 wins.
+                 2 if player 2 wins.
+         */
         if ((this.board[0][0] == this.board[1][1]) && this.board[0][0] == this.board[2][2]) {
             return this.board[0][0];
         } else if ((this.board[0][2] == this.board[1][1]) && this.board[0][2] == this.board[2][0]) {
